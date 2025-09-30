3788b518b40ceafd2e9c663fa67d1b3e617dd025
Hamza <hshaebi.11@gmail.com>
2025-09-30 02:30:19 +0100

    Initial Commit

---
+++ Kernl 00/Kernl_00App.swift
@@ -0,0 +1,17 @@
+//
+//  Kernl_00App.swift
+//  Kernl 00
+//
+//  Created by Hamza Shaebi on 30/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct Kernl_00App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
